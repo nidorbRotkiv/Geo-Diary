e07d271f508ad42808a5ef60be6182993cafc960
nidorbRotkiv <viktor.karl.brodin.1@gmail.com>
2024-07-24 14:02:43 +0200

    Return string instead of boolean

--- frontend/src/app/services/globalServices.ts
+++ frontend/src/app/services/globalServices.ts
@@ -4,6 +4,11 @@ import { BASE_API_URL } from "@/app/constants";
 import { Session } from "next-auth";
 
 export async function getMarkers(session: Session, timeout: number): Promise<any> {
+  const tokenValidityMessage = await validateSessionToken(session);
+  if (tokenValidityMessage.startsWith("Invalid")) {
+    throw new Error("Invalid session token");
+  }
+  
   const headers = new Headers({
     Authorization: `Bearer ${session.idToken}`,
     Accept: "application/json",
@@ -34,18 +39,21 @@ export async function getMarkers(session: Session, timeout: number): Promise<any
     });
 }
 
-export async function isTokenValid(session: Session): Promise<boolean> {
+export async function validateSessionToken(session: Session): Promise<string> {
   const fetchPromise = fetch(`${BASE_API_URL}/validateToken`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${session?.idToken}`,
     },
-  }).then((response) => response.ok);
+  }).then(async (response) => {
+    const responseBody = await response.json();
+    return responseBody.message;
+  });
 
-  const timeoutPromise = new Promise((resolve) => setTimeout(() => resolve(false), 6000));
+  const timeoutPromise = new Promise<string>((resolve) =>
+    setTimeout(() => resolve("Timeout: The request took too long"), 6000)
+  );
 
-  return Promise.race([fetchPromise, timeoutPromise])
-    .then((result) => result as boolean)
-    .catch(() => false);
+  return Promise.race([fetchPromise, timeoutPromise]).catch(() => "Error: Something went wrong");
 }
