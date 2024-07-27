33a0d415ecdcf0a65db768d9f68a0f7e68594251
nidorbRotkiv <viktor.karl.brodin.1@gmail.com>
2024-07-27 13:22:10 +0200

    Create AuthErrorPage

---
+++ frontend/src/app/api/auth/error/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useSearchParams } from 'next/navigation';
+import ErrorPage from '@/app/components/global/ErrorPage';
+
+const AuthErrorPage = () => {
+  const searchParams = useSearchParams();
+  const error = searchParams.get('error');
+
+  let errorMessage;
+  switch (error) {
+    case 'AccessDenied':
+      errorMessage = 'You do not have permission to access this page. This application is currently in beta. Please request access by clicking the button below.';
+      break;
+    case 'Configuration':
+      errorMessage = 'There is a problem with the server configuration.';
+      break;
+    case 'Verification':
+      errorMessage = 'The verification token is invalid or has expired.';
+      break;
+    default:
+      errorMessage = 'An unknown error occurred.';
+  }
+
+  return (
+    <ErrorPage
+      header="Authentication error"
+      text={errorMessage}
+      link={error === "AccessDenied" ? `mailto:${process.env.NEXT_PUBLIC_HOST_EMAIL_ADDRESS}?subject=Request%20for%20Access%20-%20Geo%20Diary&body=I%20wish%20to%20access%20Geo%20Diary.` : "/map"}
+      linkText={error === "AccessDenied" ? "Request access" : "Go back to Map"}
+    />
+  );
+};
+
+export default AuthErrorPage;
